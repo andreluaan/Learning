34a9105d6ba91a35ff6d924a2c7485970a989b77
αи∂яє ℓ uαи <andreluanbq@gmail.com>
2024-06-07 16:24:50 -0300

    06/07 arrow function

---
+++ 4- Função/7-arrowfunction.js
@@ -0,0 +1,18 @@
+let dobro = function (a) {
+    return 2 * a
+}
+
+dobro = (a) => {
+    return 2 * a
+}
+
+dobro = a => 2 * a // return implicito
+
+
+let ola = function () {
+    return 'olá'
+}
+
+ola = () => 'olá' //função sem parametro
+ola = _ => 'olá' // possui parametro 
+console.log(ola())
